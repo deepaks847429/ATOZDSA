7ef86aa4619d392c22f36ed1fa50c3752fdbe6ad
Delta Student <deepaks9631342845@gmail.com>
2025-09-26 22:46:04 +0530

    started with mediym problems of array

---
+++ arraymediumproblem/leadersinanarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> leaders(vector<int>& nums) {
+      
+      vector<int> res;
+      int n=nums.size();
+      res.push_back(nums[n-1]);
+      for(int i=n-2; i>=0; i--){
+        if(nums[i]>res.back()){
+            res.push_back(nums[i]);
+        }
+      }
+      reverse(res.begin(), res.end());
+      return res;
+    }
+};
